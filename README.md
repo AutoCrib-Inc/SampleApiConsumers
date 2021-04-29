a79824ed1f3100f6594309c229d691f6e05e759d
Robert Merino <rmerino@autocrib.com>
2021-04-29 14:28:45 -0700

    Initial commit of all files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# SampleApiConsumers
-Various sample starter applications that consume the Arcturus REST API. 
+# Introduction 
+Identity Server Samples are a set of an open-source projects for showing how to connect and interact with the Arcturus Web Api.
+
+# Getting Started
+To run the samples you must obtain the following from AutoCrib by emailing: wssupport@autocrib.com 
+1.	Identity Authority Url
+2.	Arcturus Web API Url
+3.	Client Id
+4. 	Client Secret
+5.	DbId
+
+
+# How to contribute
+One of the easiest ways to contribute is to participate in discussions on GitHub issues. You can also contribute by submitting pull requests with code changes.
+
+# Contact Us
+If you have questions, or you would like to reach out to us about an issue you're having or for development advice as you work on an issue, you can reach us at: wssupport@autocrib.com
---
+++ src/DotNetFramework.Client/Program.cs
@@ -0,0 +1,87 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DotNetFramework.Client
+{
+    internal class Program
+    {
+        private static async Task Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Press Enter to continue calling the API, or any other key to exit..");
+                var key = Console.ReadKey();
+
+                if (key.Key == ConsoleKey.Enter)
+                    await CallApi();
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        private static async Task CallApi()
+        {
+            // discover endpoints from metadata
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("[ENTER_THE_IDENTITY_AUTHORITY_PROVIDED_BY_AUTOCRIB]");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            Console.WriteLine("Calling IDP for Token...");
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "[ENTER_THE_IDENTITY_CLIENT_ID_PROVIDED_BY_AUTOCRIB]",
+                ClientSecret = "[ENTER_THE_IDENTITY_CLIENT_SECRET_PROVIDED_BY_AUTOCRIB]",
+                Scope = "arcturuswebapi"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine("Error calling API");
+                Console.WriteLine("\n\n");
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine("Token retrieved by IDP");
+            Console.WriteLine("\n\n");
+            Console.WriteLine(tokenResponse.Json);
+
+            // call api
+            var apiClient = new HttpClient();
+            apiClient.DefaultRequestHeaders.Add("dbId", "[ENTER_THE_DBID_PROVIDED_BY_AUTOCRIB]");
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+            Console.WriteLine("Set Bearer Token Header on Request to API...");
+            Console.WriteLine("Calling API with Bearer Token Set in Header...");
+
+            var response = await apiClient.GetAsync("[ENTER_THE_ARCTURUS_WEB_API_URL_PROVIDED_BY_AUTOCRIB]");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Error calling API");
+                Console.WriteLine(response.StatusCode);
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                Console.WriteLine("API call was successful, writing out response contents:");
+                Console.WriteLine("\n\n");
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
